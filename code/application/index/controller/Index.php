df6d23337d5e2f4571daa95df096620867cd3cbd
ceroot <ceroot@163.com>
2016-06-01 21:42:13 +0800

    修正边距

--- code/application/index/controller/Index.php
+++ code/application/index/controller/Index.php
@@ -7,7 +7,13 @@ class Index extends Controller
 {
     public function index()
     {
-        dump(ROOT_PATH);
+        dump(request()->module());
+        dump(request()->controller());
+        dump(request()->action());
+//         request()->module();//取得模块名称
+        // request()->controller()//;取得控制器名称
+        // request()->action();//取得方法名称
+        // dump(CONTROLLER_LAYER);
         return 'index';
     }
 
