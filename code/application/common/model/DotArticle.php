10bb5abe4a1e03d5fb3c64db1484f942e4b353ae
ceroot <ceroot@163.com>
2016-06-06 13:59:45 +0800

    添加文件

---
+++ code/application/common/model/DotArticle.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------+
+// | CYCMS                                                                |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2016 http://beneng.com All rights reserved.            |
+// +----------------------------------------------------------------------+
+// | Authors: SpringYang [ceroot@163.com]                                 |
+// +----------------------------------------------------------------------+
+/**
+ * @filename
+ * @authors   SpringYang
+ * @email     ceroot@163.com
+ * @QQ        525566309
+ * @date      2016-06-06 11:49:58
+ * @site      http://www.benweng.com
+ */
+namespace app\common\model;
+
+use app\common\model\Extend;
+
+class DotArticle extends Extend
+{
+
+}
---
+++ code/application/console/validate/DotArticle.php
@@ -0,0 +1,31 @@
+<?php
+// +----------------------------------------------------------------------+
+// | CYCMS                                                                |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2016 http://beneng.com All rights reserved.            |
+// +----------------------------------------------------------------------+
+// | Authors: SpringYang [ceroot@163.com]                                 |
+// +----------------------------------------------------------------------+
+/**
+ * @filename
+ * @authors   SpringYang
+ * @email     ceroot@163.com
+ * @QQ        525566309
+ * @date      2016-06-06 11:50:11
+ * @site      http://www.benweng.com
+ */
+namespace app\console\validate;
+
+use think\Validate;
+
+class DotArticle extends Validate
+{
+
+    protected $rule = [
+        'title' => 'require',
+    ];
+
+    protected $message = [
+        'title.require' => '标题必填',
+    ];
+}
