7994e2c14f1bd13d15c7648f4741f90d844a4677
ceroot <ceroot@163.com>
2016-06-07 17:19:31 +0800

    update

--- code/application/common/model/DotArticle.php
+++ code/application/common/model/DotArticle.php
@@ -17,8 +17,50 @@
 namespace app\common\model;
 
 use app\common\model\Extend;
+use think\Input;
 
-class DotArticle extends Extend
-{
+class DotArticle extends Extend {
 
+	public function datahandle() {
+
+		$data = input('post.');
+
+		// 封面保存并取得路径
+		$file = Input::file('images');
+		if ($file) {
+			$info = $file->move('./data/images/');
+			if ($info) {
+				$filename = $info->getFilename();
+				$data['cover'] = date('Ymd') . '/' . $filename;
+			}
+		}
+
+		// 关键词处理
+		if (empty($data['keywords'])) {
+			$str = strip_tags($data['content']); // 去掉 html 代码
+			$str = str_replace(' ', '', $str); // 去掉空格
+			$str = str_replace('&nbsp;', '', $str); // 去掉 &nbsp;
+			$data['keywords'] = get_keywords($str);
+		}
+
+		// 描述处理
+		if (empty($data['description'])) {
+			$pattern = '/<p[^>]*>(.*?)<\/p>/';
+			$preg = preg_match($pattern, $data['content'], $matches);
+			if ($preg) {
+				$description = strip_tags($matches[1]);
+			} else {
+				$description = mb_substr(strip_tags($data['content']), 0, 120);
+			}
+			$data['description'] = str_replace('&nbsp;', '', str_replace(' ', '', $description));
+		}
+
+		if ($data['id']) {
+			$status = $this->save($data, ['id' => $data['id']]);
+		} else {
+			$status = $this->save($data);
+		}
+
+		return $status;
+	}
 }
--- code/application/console/controller/DotArticle.php
+++ code/application/console/controller/DotArticle.php
@@ -44,55 +44,64 @@ class DotArticle extends Base {
 	}
 
 	protected function edit_before() {
-		$cid = input('cid');
-		if ($cid == '-1') {
-			return $this->error('请选择分类');
+		if (request()->isAjax()) {
+			$cid = input('cid');
+			if ($cid == '-1') {
+				return $this->error('请选择分类');
+			}
+			$id = input('post.id');
+			$data = db(CONTROLLER_NAME)->find($id);
+			$file = Input::file('images');
+			if ($file) {
+				$path = '/data/images/' . $data['cover'];
+				if (file_exists('.' . $path)) {
+					unlink('.' . $path);
+				}
+			}
+
+			$contentForm = input('post.content');
+			if (!$contentForm) {
+				return false;
+			} else {
+				$pattern = '<img.*?src="(.*?)">';
+				preg_match_all($pattern, $contentForm, $matchesForm);
+				$imgForm = $matchesForm[1];
+
+				$contentSql = $data['content'];
+				preg_match_all($pattern, $contentSql, $matchesSql);
+				$imgSql = $matchesSql[1];
+
+				foreach ($imgForm as $value) {
+					if (in_array($value, $imgSql)) {
+						$k = array_search($value, $imgSql);
+						unset($imgSql[$k]);
+					}
+				}
+
+				foreach ($imgSql as $value) {
+					$arr = parse_url($value);
+					$path = $arr['path'];
+					if (file_exists('.' . $path)) {
+						unlink('.' . $path);
+					}
+				}
+			}
 		}
 	}
 
 	public function add() {
 		if (request()->isAjax()) {
 			$data = input('post.');
-			$content = $data['content'];
 
 			$status = $this->model->validate(true);
-
 			if ($status === false) {
 				return $this->error($this->model->getError());
 			}
 
-			// 封面保存并取得路径
-			$file = Input::file('images');
-			if ($file) {
-				$info = $file->move('./data/images/');
-				if ($info) {
-					$filename = $info->getFilename();
-
-					$data['cover'] = date('Ymd') . '/' . $filename;
-				}
-			}
-
-			// 关键词处理
-			if (empty($data['keywords'])) {
-				$data['keywords'] = get_keywords(strip_tags($content));
-			}
-			// return $data;
-			// 描述处理
-			if (empty($data['description'])) {
-				$pattern = '/<p[^>]*>(.*?)<\/p>/';
-				$preg = preg_match($pattern, $content, $matches);
-				if ($preg) {
-					$description = strip_tags($matches[1]);
-				} else {
-					$description = mb_substr(strip_tags($content), 0, 120);
-				}
-				$data['description'] = $description;
-			}
-
-			$status = $this->model->save($data);
-			// return $data;
+			$status = $this->model->datahandle();
+			// return $status;
 			if ($status) {
-				action_log($status, 'dotarticle_add'); // 记录日志
+				action_log($status); // 记录日志
 				return $this->success('添加成功', url('list'));
 			} else {
 				// return $this->error('失败');
@@ -100,6 +109,36 @@ class DotArticle extends Base {
 			}
 		} else {
 
+			return $this->fetch();
+		}
+	}
+
+	public function edit() {
+		if (request()->isAjax()) {
+			$data = input('post.');
+
+			$status = $this->model->validate(true);
+			if ($status === false) {
+				return $this->error($this->model->getError());
+			}
+
+			$status = $this->model->datahandle();
+			// return $status;
+			if ($status) {
+				action_log($data['id']); // 记录日志
+				return $this->success('修改成功', url('list'));
+			} else {
+				// return $this->error('失败');
+				return $this->model->getError();
+			}
+		} else {
+			$id = input('get.id');
+			if (!$id) {
+				return $this->error('参数错误');
+			}
+
+			$one = db(CONTROLLER_NAME)->find($id);
+			$this->assign('one', $one);
 			return $this->fetch();
 		}
 	}
@@ -125,7 +164,7 @@ class DotArticle extends Base {
 	public function ppp() {
 		$str = '<p style="text-indent: 2em; text-align: left;">华为的官方解释则是：在ICT产业中，友商之间应该通过开放式的创新、联合创新的方式共同推动产业进步。业界解决知识产权问题的最佳途径，是通过谈判签订专利交叉许可协议后合法使用对方开发的技术和知识产权。诉讼是最后的解决争议的方式，在某些情况下也是一种常见方式。</p><p style="text-indent: 2em; text-align: left;">此，三星方面回应《中国经济周刊》记者称，“我们会对诉讼做彻底评估，并采取适当行动维护三星的业务利益”。截至发稿，记者未获得华为方面的采访回应。
 </p><p style="text-indent: 2em; text-align: left;">为何同时在中国、美国两地起诉</p>';
-		$pattern = '/<p[^>]*>(.*?)<\/p>/';
+		// $pattern = '/<p[^>]*>(.*?)<\/p>/';
 		$pp = preg_match($pattern, $str, $matches);
 		dump($pp);
 		dump($matches[1]);
