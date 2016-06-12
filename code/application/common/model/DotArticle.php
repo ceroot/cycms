59023ad691f99d19b458d9c827b478a36129a464
ceroot <ceroot@163.com>
2016-06-13 01:52:05 +0800

    改进封面图上传

--- code/application/common/model/DotArticle.php
+++ code/application/common/model/DotArticle.php
@@ -19,63 +19,100 @@ namespace app\common\model;
 use app\common\model\Extend;
 use think\Input;
 
-class DotArticle extends Extend {
+class DotArticle extends Extend
+{
 
-	// 写入处理
-	public function datahandle() {
-		$data = input('post.');
-		// return $data['content'];
-		$id = input('post.id'); // 因需要到 id 来判断写入形式，所以这里单独获取 id，有 id 的时候是修改，没有 id 的时候是新增
+    // 写入处理
+    public function datahandle()
+    {
+        $data = input('post.');
+        // return $data['content'];
+        $id = input('post.id'); // 因需要到 id 来判断写入形式，所以这里单独获取 id，有 id 的时候是修改，没有 id 的时候是新增
 
-		// 一些路径
-		$pathFiles = './data/files/'; // 文件保存路径
-		$pathImages = './data/images/'; // 图片保存路径
-		$pathVideos = './data/videos/'; // 视频保存路径
+        // 一些路径
+        $pathFiles  = './data/files/'; // 文件保存路径
+        $pathImages = './data/images/'; // 图片保存路径
+        $pathVideos = './data/videos/'; // 视频保存路径
+        $pathTemp   = './data/temp/'; // 临时文件存放路径
 
-		// 封面保存并取得路径
-		$file = Input::file('cover');
-		if ($file) {
-			// 修改时先删除原有图片处理
-			if ($id) {
-				$coveSql = $this->getFieldById($id, 'cover');
+        // 封面保存并取得路径
+        $file = Input::file('cover');
+        if ($file) {
+            // 修改时先删除原有图片处理
+            if ($id) {
+                $coveSql = $this->getFieldById($id, 'cover');
 
-				$pathSql = $pathImages . $coveSql;
-				if (is_file($pathSql)) {
-					unlink($pathSql);
-				}
-			}
+                $pathSql = $pathImages . $coveSql;
+                if (is_file($pathSql)) {
+                    unlink($pathSql);
+                }
+            }
 
-			// 图片保存
-			$info = $file->move($pathImages);
-			if ($info) {
-				$filename = $info->getFilename();
-				$data['cover'] = date('Ymd') . '/' . $filename;
-			}
-		} else {
-			unset($data['cover']); // 如果没有数据则删除数组值
-		}
+            // 封面图片保存
+            $info = $file->move($pathTemp);
+            if ($info) {
+                $filename = $info->getFilename();
+                $ymd      = date('Ymd') . '/';
+                $tempFile = $pathTemp . $ymd . $filename;
+                $savePath = $pathImages . $ymd . $filename;
+                if (!file_exists($pathImages . $ymd)) {
+                    make_dir($pathImages . $ymd);
+                }
+                // 取得文件信息
+                $imageInfo = getimagesize($tempFile);
+                if ($imageInfo) {
+                    // 判断图片宽度来是否进行缩小操作
+                    if ($imageInfo[0] > 800) {
+                        if (is_file($tempFile)) {
+                            // 实例化图片尺寸类
+                            $newimage = new \imageresize\ImageResize();
+                            $result   = $newimage->resize($tempFile, $savePath, 800, 500);
+                            if ($result) {
+                                $cover = $ymd . $filename;
+                                unlink($tempFile); // 删除临时文件
+                            }
+                        }
+                    } else {
+                        // 移动文件
+                        $result = rename($tempFile, $savePath);
+                        if ($result) {
+                            $cover = $ymd . $filename;
+                        }
+                    }
+                }
 
-		// 关键词处理
-		if (empty($data['keywords'])) {
-			$data['keywords'] = get_keywords($data['content']);
-		}
+                // 如果取得则给数组加cover值
+                if ($cover) {
+                    $data['cover'] = $ymd . $filename;
+                } else {
+                    unset($data['cover']); // 如果没有数据则删除数组值
+                }
+            }
+        } else {
+            unset($data['cover']); // 如果没有数据则删除数组值
+        }
 
-		// 描述处理
-		if (empty($data['description'])) {
-			$data['description'] = get_description($data['content']);
-		}
+        // 关键词处理
+        if (empty($data['keywords'])) {
+            $data['keywords'] = get_keywords($data['content']);
+        }
 
-		// 对 ueditor 内容数据的处理
-		$data['content'] = ueditor_handle($data['content'], $data['title']);
+        // 描述处理
+        if (empty($data['description'])) {
+            $data['description'] = get_description($data['content']);
+        }
 
-		// 写入形式
-		if ($id) {
-			$status = $this->save($data, ['id' => $id]);
-		} else {
-			$status = $this->save($data);
-		}
+        // 对 ueditor 内容数据的处理
+        $data['content'] = ueditor_handle($data['content'], $data['title']);
 
-		// 返回状态
-		return $status;
-	}
+        // 写入形式
+        if ($id) {
+            $status = $this->save($data, ['id' => $id]);
+        } else {
+            $status = $this->save($data);
+        }
+
+        // 返回状态
+        return $status;
+    }
 }
--- code/application/console/controller/DotArticle.php
+++ code/application/console/controller/DotArticle.php
@@ -19,114 +19,129 @@ namespace app\console\controller;
 use app\console\controller\Base;
 use think\Input;
 
-class DotArticle extends Base {
-	/**
-	 * [_initialize 初始化]
-	 * @return [type] [description]
-	 */
-	public function _initialize() {
-		parent::_initialize();
-		$category = model('dotCategory')->getAll();
-		$this->assign('category', $category);
-	}
+class DotArticle extends Base
+{
+    /**
+     * [_initialize 初始化]
+     * @return [type] [description]
+     */
+    public function _initialize()
+    {
+        parent::_initialize();
+        $category = model('dotCategory')->getAll();
+        $this->assign('category', $category);
+    }
 
-	protected $beforeActionList = [
-		'add_before' => ['only' => 'add'],
-		'edit_before' => ['only' => 'edit'],
-	];
+    protected $beforeActionList = [
+        'add_before'  => ['only' => 'add'],
+        'edit_before' => ['only' => 'edit'],
+    ];
 
-	protected function add_before() {
+    protected function add_before()
+    {
 
-	}
+    }
 
-	protected function edit_before() {
+    protected function edit_before()
+    {
 
-	}
+    }
 
-	public function add() {
-		if (request()->isAjax()) {
-			$data = input('post.');
-			$result = $this->validate($data, request()->controller());
+    public function add()
+    {
+        if (request()->isAjax()) {
+            $data   = input('post.');
+            $result = $this->validate($data, request()->controller());
 
-			if ($result !== true) {
-				return $this->error($result);
-			}
+            if ($result !== true) {
+                return $this->error($result);
+            }
 
-			$status = $this->model->datahandle();
-			if ($status) {
-				action_log($status); // 记录日志
-				return $this->success('添加成功', url('list'));
-			} else {
-				return $this->model->getError();
-			}
-		} else {
-			return $this->fetch();
-		}
-	}
+            $status = $this->model->datahandle();
+            return $status;
+            if ($status) {
+                action_log($status); // 记录日志
+                return $this->success('添加成功', url('list'));
+            } else {
+                return $this->model->getError();
+            }
+        } else {
+            return $this->fetch();
+        }
+    }
 
-	public function edit() {
-		$method = strtolower(request()->method());
+    public function edit()
+    {
+        $method = strtolower(request()->method());
 
-		switch ($method) {
-		case 'get':
-			$id = input('get.id');
-			break;
-		case 'post':
-			$id = input('post.id');
-			break;
-		default:
-			return $this->error('提交有误，请重试');
-			break;
-		}
+        switch ($method) {
+            case 'get':
+                $id = input('get.id');
+                break;
+            case 'post':
+                $id = input('post.id');
+                break;
+            default:
+                return $this->error('提交有误，请重试');
+                break;
+        }
 
-		if (!$id) {
-			return $this->error('参数错误');
-		}
+        if (!$id) {
+            return $this->error('参数错误');
+        }
 
-		// 读取数据
-		$one = db(request()->controller())->find($id);
+        // 读取数据
+        $one = db(request()->controller())->find($id);
 
-		// 判断提交类型
-		if (request()->isAjax()) {
-			$data = input('post.');
-			$result = $this->validate($data, request()->controller());
+        // 判断提交类型
+        if (request()->isAjax()) {
+            $data   = input('post.');
+            $result = $this->validate($data, request()->controller());
 
-			if ($result !== true) {
-				return $this->error($result);
-			}
+            if ($result !== true) {
+                return $this->error($result);
+            }
 
-			// 封面处理
-			$file = Input::file('images');
-			if ($file) {
-				if (empty($one['cover'])) {
-					$path = '/data/images/' . $one['cover'];
-					if (file_exists('.' . $path)) {
-						unlink('.' . $path);
-					}
-				}
-			}
+            // 修改时对封面的处理
+            $file = Input::file('images');
+            if ($file) {
+                if (empty($one['cover'])) {
+                    $path = '/data/images/' . $one['cover'];
+                    if (file_exists('.' . $path)) {
+                        unlink('.' . $path);
+                    }
+                }
+            }
 
-			$contentForm = $data['content'];
-			if ($contentForm) {
-				$contentSql = $one['content'];
-				if (empty($contentSql)) {
-					// 对比判断并删除操作
-					del_file($contentForm, $contentSql);
-				}
-			}
+            $contentForm = $data['content'];
+            if ($contentForm) {
+                $contentSql = $one['content'];
+                if (empty($contentSql)) {
+                    // 对比判断并删除操作
+                    del_file($contentForm, $contentSql);
+                }
+            }
 
-			$status = $this->model->datahandle();
-			// return $status;
-			if ($status) {
-				action_log($data['id']); // 记录日志
-				return $this->success('修改成功', url('list'));
-			} else {
-				return $this->model->getError();
-			}
-		} else {
-			$this->assign('one', $one);
-			return $this->fetch();
-		}
-	}
+            $status = $this->model->datahandle();
+            // return $status;
+            if ($status) {
+                action_log($data['id']); // 记录日志
+                return $this->success('修改成功', url('list'));
+            } else {
+                return $this->model->getError();
+            }
+        } else {
+            $this->assign('one', $one);
+            return $this->fetch();
+        }
+    }
+
+    public function images()
+    {
+        $newimage = new \imageresize\ImageResize();
+        $dd       = $newimage->resize("./data/images/20160612/d25d8002beafe722f45ab46c5343bac6.jpg", "./data/images/20160612/test_400_400.jpg", 800, 500);
+        dump($dd);
+
+    }
 
 }
