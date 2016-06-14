64b76c34fe526586a4b844d711ae3a247591cf1e
ceroot <ceroot@163.com>
2016-06-14 17:08:37 +0800

    添加文件

---
+++ code/application/common/behavior/ModuleInit.php
@@ -0,0 +1,36 @@
+<?php
+// +----------------------------------------------------------------------+
+// | CYCMS                                                                |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2016 http://beneng.com All rights reserved.            |
+// +----------------------------------------------------------------------+
+// | Authors: SpringYang [ceroot@163.com]                                 |
+// +----------------------------------------------------------------------+
+/**
+ * @filename
+ * @authors   SpringYang
+ * @email     ceroot@163.com
+ * @QQ        525566309
+ * @date      2016-06-14 16:45:14
+ * @site      http://www.benweng.com
+ */
+namespace app\common\behavior;
+
+class ModuleInit
+{
+    public function run(&$params)
+    {
+        $this->initialization();
+
+    }
+
+    //初始化
+    private function initialization()
+    {
+        define('MODULE_NAME', request()->module());
+        define('CONTROLLER_NAME', request()->controller());
+        define('ACTION_NAME', request()->action());
+
+    }
+
+}
