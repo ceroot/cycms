536744d0f18c1aa4ad87922ea2a2ba775ea3f939
ceroot <ceroot@163.com>
2016-06-15 23:11:16 +0800

    添加文件

---
+++ code/application/common/model/OauthUser.php
@@ -0,0 +1,25 @@
+<?php
+// +----------------------------------------------------------------------+
+// | CYCMS                                                                |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2016 http://beneng.com All rights reserved.            |
+// +----------------------------------------------------------------------+
+// | Authors: SpringYang [ceroot@163.com]                                 |
+// +----------------------------------------------------------------------+
+/**
+ * @filename
+ * @authors   SpringYang
+ * @email     ceroot@163.com
+ * @QQ        525566309
+ * @date      2016-06-15 22:23:27
+ * @site      http://www.benweng.com
+ */
+
+namespace app\common\model;
+
+use app\common\model\Extend;
+
+class OauthUser extends Extend
+{
+
+}
