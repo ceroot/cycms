70a35e8957f11af348910abbd1fb54c2444bf026
ceroot <ceroot@163.com>
2016-07-01 07:48:38 +0800

    添加文件

---
+++ code/application/common/model/Joke.php
@@ -0,0 +1,25 @@
+<?php
+// +----------------------------------------------------------------------+
+// | CYCMS                                                                |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2016 http://beneng.com All rights reserved.            |
+// +----------------------------------------------------------------------+
+// | Authors: SpringYang [ceroot@163.com]                                 |
+// +----------------------------------------------------------------------+
+/**
+ * @filename
+ * @authors   SpringYang
+ * @email     ceroot@163.com
+ * @QQ        525566309
+ * @date      2016-06-30 23:17:28
+ * @site      http://www.benweng.com
+ */
+
+namespace app\common\model;
+
+use app\common\model\Extend;
+
+class Joke extends Extend
+{
+
+}
---
+++ code/application/console/controller/Joke.php
@@ -0,0 +1,30 @@
+<?php
+// +----------------------------------------------------------------------+
+// | CYCMS                                                                |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2016 http://beneng.com All rights reserved.            |
+// +----------------------------------------------------------------------+
+// | Authors: SpringYang [ceroot@163.com]                                 |
+// +----------------------------------------------------------------------+
+/**
+ * @filename
+ * @authors   SpringYang
+ * @email     ceroot@163.com
+ * @QQ        525566309
+ * @date      2016-06-30 23:16:15
+ * @site      http://www.benweng.com
+ */
+
+namespace app\console\controller;
+
+class Joke extends Base
+{
+    /**
+     * [_initialize 初始化]
+     * @return [type] [description]
+     */
+    public function _initialize()
+    {
+        parent::_initialize();
+    }
+}
